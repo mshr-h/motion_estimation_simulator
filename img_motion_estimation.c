a709fff82e142928b10df751f6567134e235ce7e
mshr-h <mshr-h@users.noreply.github.com>
2015-02-13 18:32:26 +0900

    fix print result

--- img_motion_estimation.c
+++ img_motion_estimation.c
@@ -2,36 +2,35 @@
 #include "include/motion_estimation.h"
 
 void
-img_motion_estimation(
+img_motion_estimation (
 	struct img_rgb_t *prev_image,
 	struct img_rgb_t *curr_image,
 	int tb_size,
 	int sw_size
 )
 {
-	int average;        // average of SAD
+	int result;
 
-	average = fullsearch_8bit(
-				  prev_image,
-				  curr_image,
-				  tb_size,
-				  sw_size);
-	fprintf(stdout, "Full search                  : %d\n", average);
-	fflush(stdout);
+	result = fullsearch_8bit(
+				 prev_image,
+				 curr_image,
+				 tb_size,
+				 sw_size);
+	printf("Full search 8bit              : %d\n", result);
 
-	average = fullsearch_4bit_4pix(
-				  prev_image,
-				  curr_image,
-				  tb_size,
-				  sw_size);
-	fprintf(stdout, "Full search 4bit 4pix        : %d\n", average);
-	fflush(stdout);
+	result = fullsearch_4bit_4pix(
+				 prev_image,
+				 curr_image,
+				 tb_size,
+				 sw_size);
+	printf("Full search 4bit 4pix         : %d\n", result);
 
-	average = fullsearch_4bit_4pix_dynamic(
-				  prev_image,
-				  curr_image,
-				  tb_size,
-				  sw_size);
-	fprintf(stdout, "Full search 4bit 4pix dynamic: %d\n", average);
+	result = fullsearch_4bit_4pix_dynamic(
+				 prev_image,
+				 curr_image,
+				 tb_size,
+				 sw_size);
+	printf("Full search 4bit 4pix dynamic : %d\n", result);
+
 	fflush(stdout);
 }
