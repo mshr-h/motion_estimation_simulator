75ff4ddd9014bd14dea516903e5636f708800405
mshr-h <mshr-h@users.noreply.github.com>
2015-02-22 03:52:19 +0900

    [fix] free

--- implab.c
+++ implab.c
@@ -37,9 +37,9 @@ img_rgb_destruct(
 	struct img_rgb_t *img
 )
 {
-	free(img->r);
-	free(img->g);
-	free(img->b);
+	free(img->r[0]); free(img->r);
+	free(img->g[0]); free(img->g);
+	free(img->b[0]); free(img->b);
 	free(img);
 }
 
