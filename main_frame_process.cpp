538c64bd16c9e6497574992b5bc7a2339e768ff2
mshr-h <mshr-h@users.noreply.github.com>
2016-02-11 18:13:20 +0900

    fix main

--- main_frame_process.cpp
+++ main_frame_process.cpp
@@ -1,5 +1,19 @@
-
 #include "include/main.h"
+
+void printResult(struct img_t *img_curr, struct img_t *img_prev, void (*method)(struct me_block_t *, unsigned char (*)(unsigned char, unsigned char)), const char *name)
+{
+    auto me_block=me_block_create(img_curr, img_prev, 16, 16);
+    method(me_block, pe_8bit_diff);
+    auto img_rec=me_block_reconstruct(me_block);
+    printf("%s: %6d, sad: %f, psnr: %f\n",
+           name,
+           me_block_calc_sum_cost_match(me_block),
+           me_block_calc_average_cost(me_block),
+           img_psnr(img_curr, img_rec));
+    fflush(stdout);
+    img_destruct(img_rec);
+    me_block_destruct(me_block);
+}
 
 int main_frame_process(int argc, char *argv[])
 {
@@ -38,61 +52,13 @@ int main_frame_process(int argc, char *argv[])
     img_yuv_destruct(curr_img_yuv);
     img_yuv_destruct(prev_img_yuv);
 
-    auto me_block_fullsearch=me_block_create(img_curr, img_prev, 16, 16);
-    auto me_block_fullsearch_4pix=me_block_create(img_curr, img_prev, 16, 16);
-    auto me_block_fullsearch_4pix_only=me_block_create(img_curr, img_prev, 16, 16);
-    auto me_block_fullsearch_edge1=me_block_create(img_curr, img_prev, 16, 16);
-    auto me_block_fullsearch_edge2=me_block_create(img_curr, img_prev, 16, 16);
-    auto me_block_fullsearch_edge3=me_block_create(img_curr, img_prev, 16, 16);
-    auto me_block_fullsearch_matching=me_block_create(img_curr, img_prev, 16, 16);
+    printResult(img_curr, img_prev, fullsearch,           "full     ");
+    printResult(img_curr, img_prev, fullsearch_4pix,      "full4pix ");
+    printResult(img_curr, img_prev, fullsearch_4pix_only, "full4pixO");
+    printResult(img_curr, img_prev, fullsearch_matching,  "fullmatch");
+
     img_destruct(img_curr);
     img_destruct(img_prev);
-
-    // current method
-    fullsearch(me_block_fullsearch, pe_8bit_diff);
-    printf("curr : %6d, sad: %f\n",
-           me_block_calc_sum_cost_match(me_block_fullsearch),
-           me_block_calc_average_cost(me_block_fullsearch));
-    fflush(stdout);
-
-    // 4pix search method
-    fullsearch_4pix(me_block_fullsearch_4pix, pe_8bit_diff);
-    printf("4pix : %6d, sad: %f\n",
-           me_block_calc_sum_cost_match(me_block_fullsearch_4pix),
-           me_block_calc_average_cost(me_block_fullsearch_4pix));
-    fflush(stdout);
-
-    // 4pix only search method
-    fullsearch_4pix_only(me_block_fullsearch_4pix_only, pe_8bit_diff);
-    printf("4pixo: %6d, sad: %f\n",
-           me_block_calc_sum_cost_match(me_block_fullsearch_4pix_only),
-           me_block_calc_average_cost(me_block_fullsearch_4pix_only));
-    fflush(stdout);
-
-    // proposed method 4
-    fullsearch_matching(me_block_fullsearch_matching, pe_8bit_diff);
-    printf("match: %6d, sad: %f\n",
-           me_block_calc_sum_cost_match(me_block_fullsearch_matching),
-           me_block_calc_average_cost(me_block_fullsearch_matching));
-    fflush(stdout);
-
-    printf("%.2f sec\n",  (double)(clock()-start)/CLOCKS_PER_SEC);
-
-    /*FILE *fp_out_csv=fopen(argv[3], "w");
-    if(NULL==fp_out_csv)
-    {
-        fprintf(stderr, "%s:%d cannot open %s\n", __FILE__, __LINE__ , argv[3]);
-        exit(1);
-    }
-
-    fclose(fp_out_csv);*/
-
-    me_block_destruct(me_block_fullsearch);
-    me_block_destruct(me_block_fullsearch_4pix);
-    me_block_destruct(me_block_fullsearch_edge1);
-    me_block_destruct(me_block_fullsearch_edge2);
-    me_block_destruct(me_block_fullsearch_edge3);
-    me_block_destruct(me_block_fullsearch_matching);
 
     return 0;
 }
